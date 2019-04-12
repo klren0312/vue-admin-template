811edb902c05f60f8e8310884e444611ddc312c3
zzes <kuanglong0312@sina.com>
2019-04-12 14:37:09 +0800

    del

--- vue.config.js
+++ vue.config.js
@@ -20,16 +20,6 @@ module.exports = {
       threshold: 10240,
       minRatio: 0.8
     }))
-    config.plugins.push(new UglifyJsPlugin({
-      uglifyOptions: {
-        compress: {
-          warnings: false,
-          drop_debugger: true,
-          drop_console: true,
-        },
-      },
-      sourceMap: false,
-      parallel: true,
-    }))
+  
   }
 }
